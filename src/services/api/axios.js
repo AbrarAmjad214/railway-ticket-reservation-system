acea0f3cb45eee37c441b2cc631e2847d7ca94c5
AbrarAmjad214 <ranabobi30@gmail.com>
2026-01-16 04:10:38 +0500

    Change Frontend Vercel URL and Add api at the end

--- src/services/api/axios.js
+++ src/services/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API_BASE_URL =
   // import.meta.env.VITE_API_URL || "http://localhost:5000/api";
-  import.meta.env.VITE_API_URL || "https://railway-ticket-reservation-system-b.vercel.app";
+  import.meta.env.VITE_API_URL || "https://railway-ticket-reservation-system-b.vercel.app/api";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
